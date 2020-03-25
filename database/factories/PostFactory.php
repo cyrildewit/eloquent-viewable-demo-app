9e77492c170d78f67aa948dbea2a5c4a091a9f2c
Cyril de Wit <cyrilwit@gmail.com>
2020-03-25 09:33:29 +0100

    feat: add PostFactory

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Post;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+use Illuminate\Support\Arr;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(Post::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence,
+        'body' => implode(" ", $faker->paragraphs(12)),
+    ];
+});
